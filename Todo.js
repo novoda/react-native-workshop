d7e335fe50142c26a46cd881812779e65091ccf8
Daniele Conti <daniele.conti@novoda.com>
2018-07-02 11:53:23 +0100

    Show switch in todo component

--- Todo.js
+++ Todo.js
@@ -1,3 +1,15 @@
 import React from "react";
-import {Text} from "react-native";
-export const Todo = ({name}) => (<Text>{name}</Text>);
+import { View, Switch, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row'
+    }
+})
+
+export const Todo = ({ name, completed }) => (
+    <View style={styles.container}>
+        <Text>{name}</Text>
+        <Switch value={completed} />
+    </View>
+);
