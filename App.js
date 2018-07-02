116028d53fcf403cf9f70d9098d612e650ee3ed1
Daniele Conti <daniele.conti@novoda.com>
2018-07-02 14:03:51 +0100

    Load todos on app start

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, AsyncStorage } from 'react-native';
 import { TodoList } from './TodoList';
+
+const defaultTodos = [
+  {
+    name: "Take the dog out",
+    completed: false,
+  },
+  {
+    name: "Go to the gym",
+    completed: true,
+  },
+  {
+    name: "Prepare dinner",
+    completed: false
+  }
+];
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      todos: [
-        {
-          name: "Take the dog out",
-          completed: false,
-        },
-        {
-          name: "Go to the gym",
-          completed: true,
-        },
-        {
-          name: "Prepare dinner",
-          completed: false
-        }
-      ]
-    };
+    this.state = { todos: defaultTodos };
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem('todos').then(todosJson => {
+      if (todosJson) {
+        this.setState({ todos: JSON.parse(todosJson) })
+      }
+    })
   }
 
   onTodoChanged = ({ completed, index }) => {
