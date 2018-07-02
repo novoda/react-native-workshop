1d9b8650b81121d17c47da7c1ea6abdb96dd3f3b
Daniele Conti <daniele.conti@novoda.com>
2018-07-02 13:01:43 +0100

    Move todos to state

--- App.js
+++ App.js
@@ -2,26 +2,32 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { TodoList } from './TodoList';
 
-const todos = [
-  {
-    name: "Take the dog out",
-    completed: false,
-  },
-  {
-    name: "Go to the gym",
-    completed: true,
-  },
-  {
-    name: "Prepare dinner",
-    completed: false
-  }
-];
-
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          name: "Take the dog out",
+          completed: false,
+        },
+        {
+          name: "Go to the gym",
+          completed: true,
+        },
+        {
+          name: "Prepare dinner",
+          completed: false
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <TodoList todos={todos} />
+        <TodoList todos={this.state.todos} />
       </View>
     );
   }
